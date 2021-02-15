f8a7fae9500370dcbcef922df0e5f4d002352939
Kaleb Byrum <33094708+kabyru@users.noreply.github.com>
2021-02-15 11:04:49 -0500

    Update dataReshape.py

--- dataReshape.py
+++ dataReshape.py
@@ -1,5 +1,7 @@
 import csv
+import numpy as np
 
+#Import and convert the CSV to Tensor format###############################################
 with open('Real-estate-valuation-data-set.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
@@ -15,6 +17,50 @@ for i in range(1,len(data)):
     extractDataOutputs.append(data[i][7])
 
 print(extractDataOutputs)
+
+################################################
+
+#Extract 80% of the data for training, keep 20% of the data for testing.#################
+
+#First, the inputs#######################################################
+lenData = len(extractDataInputs)
+trainingLen = int(0.8*lenData)
+testLen = lenData - trainingLen
+
+trainDataInputs = []
+for i in range(0,trainingLen):
+    trainDataInputs.append(extractDataInputs[i])
+
+print(trainDataInputs)
+
+testDataInputs = []
+for i in range(trainingLen,lenData):
+    testDataInputs.append(extractDataInputs[i])
+
+print(testDataInputs)
+
+#Next, the outputs########################################################
+lenData = len(extractDataOutputs)
+trainingLen = int(0.8*lenData)
+testLen = lenData - trainingLen
+
+
+trainDataOutputs = []
+for i in range(0,trainingLen):
+    trainDataOutputs.append(extractDataOutputs[i])
+
+print(trainDataOutputs)
+
+testDataOutputs = []
+for i in range(trainingLen,lenData):
+    testDataOutputs.append(extractDataOutputs[i])
+
+print(testDataOutputs)
+#########################################################################
+
+
+
+
 
 #https://stackoverflow.com/questions/26850355/converting-list-to-numpy-array
 #To-Do: convert these lists into numpy arrays for Keras input. See: Lab 1 Code Data Reshaping
